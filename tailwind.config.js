0c2c835cec5eabdba9bc66a0864226dfac2c8b81
Chhun <Srykimchhun9@gmail.com>
2022-11-04 16:29:57 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily : {
+        'change' : "'Roboto', sans-serif",
+      }
+     },
+  },
+  plugins: [],
+}
